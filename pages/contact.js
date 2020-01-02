1ef745f5e01f4e59297d76bae722775d9b45aa0b
heath-laptop <heath.blandford@gmail.com>
2020-01-01 20:50:57 -0500

    add smol para to contat

--- pages/contact.js
+++ pages/contact.js
@@ -5,27 +5,36 @@ export default function docs() {
     return (
         <Layout>
             <Head>
-                <title>
-                    Contact me... or don't I don't care
-                </title>
+                <title>Contact me... or don't I don't care</title>
                 <meta
                     name="viewport"
                     content="initial-scale=1.0, width=device-width"
                 />
             </Head>
+            <div>
+                <p>
+                    The best way to contact me is probably{" "}
+                    <a href="https://twitter.com/whoisheath_">Twitter</a>. But,
+                    if you fill out the form, a notification will go off in my
+                    own discord server and I'll get back to you as quickly as
+                    possible!
+                </p>
+            </div>
             <form>
-                Name:<br/>
-                <input type="text" name="firstname" autoFocus /><br/>
-
-                Email:<br/>
-                <input type="email" name="email" /><br/>
-
-                Message:<br/>
-                <textarea rows="4" cols="32"/>
+                Name:
+                <br />
+                <input type="text" name="firstname" autoFocus />
+                <br />
+                Email:
+                <br />
+                <input type="email" name="email" />
+                <br />
+                Message:
+                <br />
+                <textarea rows="4" cols="32" />
                 <br />
                 <button type="submit">Contact Me</button>
             </form>
-            
         </Layout>
     );
 }
