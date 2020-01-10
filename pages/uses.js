fa62c4fca6f26a1fcb458c2b1b94d17d3da15e9b
heath-laptop <heath.blandford@gmail.com>
2020-01-10 14:54:50 -0500

    added uses page, to fill out later

---
+++ pages/uses.js
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout";
+import Head from "next/head";
+
+export default function uses() {
+    return (
+        <Layout>
+            <Head>
+                <title>Stuff I use!</title>
+            </Head>
+            <h1>These are the things I use!</h1>
+        </Layout>
+    )
+}
